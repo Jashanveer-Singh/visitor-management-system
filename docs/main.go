dfe27961e383afd6d8c35be291c47426964c13c0
Jashanveer-Singh <puniajashan85@gmail.com>
2025-08-25 11:43:44 +0000

    refactored api docs for issues

---
+++ docs/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+type Handler struct{}
+
+func (h *Handler) ServeOpenAPISpec(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, filepath.Join("docs", "openapi.yaml"))
+}
+
+func (h *Handler) SwaggerUIHandler() http.Handler {
+	return http.StripPrefix("/docs/", http.FileServer(http.Dir("docs/swagger")))
+}
+
+func (h *Handler) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+type Router struct{}
+
+func NewRouter() *Router {
+	return &Router{}
+}
+
+func (r *Router) Route() *http.ServeMux {
+	mux := http.NewServeMux()
+	// Initialize the handler with the app instance
+	h := &Handler{}
+
+	// docs
+	mux.HandleFunc("/docs/openapi.yaml", h.ServeOpenAPISpec)
+	mux.Handle("/docs/", h.SwaggerUIHandler())
+
+	return mux
+}
+
+func main() {
+	route := NewRouter().Route()
+	// Start the HTTP server
+	log.Println("Starting server on :8080")
+	log.Println("view docs on http://localhost:8080/docs/")
+	log.Fatal(http.ListenAndServe(":8080", route))
+}
